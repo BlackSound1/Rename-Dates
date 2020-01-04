1cfa5cae745f0ac645e8cf1e041a2fc857342d90
BlackSound1 <matthewr@videotron.ca>
2020-01-04 00:05:45 -0500

    Update renameDates.py

--- renameDates.py
+++ renameDates.py
@@ -41,22 +41,3 @@ for amerFilename in os.listdir('.'):
     # Rename the files
     print('Renaming "%s" to "%s"...' % (amerFilename, euroFilename))
     shutil.move(amerFilename, euroFilename) # uncomment after testing
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-   
-
-    
