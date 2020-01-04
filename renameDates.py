debab9b4e5dd1c52e17100440b83ea4f3e549bc8
BlackSound1 <matthewr@videotron.ca>
2020-01-04 00:05:15 -0500

    Update renameDates.py

--- renameDates.py
+++ renameDates.py
@@ -1,5 +1,5 @@
 #! python3
-# renameDates.py - Ranmes filenames with American MM-DD-YYYY date format
+# renameDates.py - Renames filenames with American MM-DD-YYYY date format
 # to European DD-MM-YYYY
 
 import shutil, os, re
